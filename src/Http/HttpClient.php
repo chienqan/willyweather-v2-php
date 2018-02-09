e6f1bbb13effc45233c156558a7e264c527ca60f
Chien Nguyen <deirpes@gmail.com>
2018-02-09 23:48:25 +0700

    init

---
+++ src/Http/HttpClient.php
@@ -0,0 +1,80 @@
+<?php
+    namespace WillyWeather\Http;
+    
+    use GuzzleHttp\Psr7\Response;
+    use GuzzleHttp\Client as GuzzleClient;
+    use GuzzleHttp\HandlerStack;
+    use Kevinrob\GuzzleCache\CacheMiddleware;
+    use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
+    use Kevinrob\GuzzleCache\Storage\FlysystemStorage;
+    use League\Flysystem\Adapter\Local;
+    
+    class HttpClient
+    {
+        protected $baseUrl = "https://api.willyweather.com.au/v2/";
+        
+        protected $apiKey;
+        
+        protected $client;
+        
+        public function __construct($apiKey, $cachePath = null)
+        {
+            $this->apiKey = $apiKey;
+            $this->cachePath = $cachePath;
+            $this->client = new GuzzleClient(["handler" => $this->getHandlerStack($cachePath)]);
+        }
+        
+        /**
+         * Perform get request.
+         * 
+         * @access public
+         * @param string $path
+         * @param array $parameters (default: [])
+         * @return array
+         */
+        public function get($path, $parameters = [])
+        {   
+            $response = $this->client->request("GET", $this->baseUrl.$this->apiKey."/".$path, ["query" => $parameters]);
+            
+            return $this->processResponse($response);
+        }
+        
+        /**
+         * processResponse function.
+         * 
+         * @access protected
+         * @param Response $response
+         * @return array
+         */
+        protected function processResponse(Response $response)
+        {
+            $body = $response->getBody(true);
+            
+            return json_decode($body, true);
+        }
+    
+        /**
+         * Return middleware handler stack with cacheing middleware added.
+         * 
+         * @access protected
+         * @return GuzzleHttp\HandlerStack
+         */
+        protected function getHandlerStack($cachePath)
+        {
+            $stack = HandlerStack::create();
+            
+            if (isset($cachePath)) {
+                $stack->push(new CacheMiddleware(
+                    new GreedyCacheStrategy(
+                        new FlysystemStorage(
+                            new Local($cachePath)
+                        ),
+                        (60 * 60 * 3) // TTL 3 hours
+                    )
+                ), "cache");
+            }
+            
+            return $stack;
+        }
+    }
+    
---
+++ src/Location.php
@@ -0,0 +1,192 @@
+<?php
+    namespace WillyWeather;
+    
+    use Carbon\Carbon;
+    
+    class Location
+    {    
+        /**
+         * client
+         * 
+         * @var WillyWeather\Client
+         * @access protected
+         */
+        protected $client;
+        
+        /**
+         * id
+         * 
+         * @var int
+         * @access protected
+         */
+        protected $id;
+        
+        protected $name;
+        
+        protected $region;
+        
+        protected $state;
+        
+        protected $postcode;
+        
+        protected $timeZone;
+        
+        protected $coordinates;
+        
+        protected $typeId;
+        
+        protected $distance;
+        
+        protected $forecasts;
+        
+        protected $observational;
+        
+        public function __construct(Client $client, $id, $weather = [], $defaultValues = [])
+        {
+            $this->client = $client;
+            
+            if (!empty($defaultValues)) {
+                $this->id = $defaultValues["id"];
+                $this->processResult(["location" => $defaultValues]);
+            } else {
+                $this->id = $id;
+                $this->fetch($weather);   
+            }
+        }
+        
+        protected function fetch($weather)
+        {
+            $params = [];
+            
+            if (!empty($weather)) {
+                if (isset($weather["days"])){
+                    $params["days"] = $weather["days"];
+                }
+                if (isset($weather["forecasts"])) {
+                    $params["forecasts"] = implode(",", $weather["forecasts"]);
+                }
+                
+                if (isset($weather["observationalGraphs"])) {
+                    $params["observationalGraphs"] = implode(",", $weather["observationalGraphs"]);
+                }
+
+                if (isset($weather["observational"]) && $weather["observational"] == true) {
+                    $params["observational"] = "true";
+                }
+
+            }
+            
+            // Hardcode metric units
+            $params["units"] = "amount:mm,distance:km,speed:km/h,swellHeight:m,temperature:c,tideHeight:m";
+            
+            $path = 'locations/'.$this->id.'/weather.json';
+            
+            $this->processResult($this->client->httpClient->get($path, $params));
+        }
+        
+        protected function processResult($result)
+        {
+            if(isset($result["location"])) {
+                $this->name = $result["location"]["name"] ?? null;
+                $this->region = $result["location"]["region"] ?? null;
+                $this->state = $result["location"]["state"] ?? null;
+                $this->postcode = $result["location"]["postcode"] ?? null;
+                $this->timeZone = $result["location"]["timeZone"] ?? null;
+                $this->coordinates = ["lat" => $result["location"]["lat"] ?? null, "lng" => $result["location"]["lng"] ?? null];
+                $this->typeId = $result["location"]["typeId"] ?? null;
+                $this->distance = $result["location"]["distance"] ?? null;
+            }
+            
+            $result = $this->convertDateTimeStrings($result);
+            
+            if(isset($result["forecasts"])) {
+                $this->forecasts = $result["forecasts"];
+            }
+            
+            if(isset($result["observationalGraphs"])) {
+                $this->observationalGraphs = $result["observationalGraphs"];
+            }
+
+            if(isset($result["observational"])) {
+                $this->observational = $result["observational"];
+            }
+        }
+        
+        protected function convertDateTimeStrings($result)
+        {
+            array_walk_recursive($result, function(&$value) {
+                if (preg_match("/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/", $value)) {
+                    $value = Carbon::createFromFormat('Y-m-d H:i:s', $value, $this->timeZone);
+                }
+            });
+            
+            return $result;
+        }
+        
+        public function getName()
+        {
+            return $this->name;
+        }
+        
+        public function getRegion()
+        {
+            return $this->region;
+        }
+        
+        public function getState()
+        {
+            return $this->state;
+        }
+        
+        public function getPostcode()
+        {
+            return $this->postcode;
+        }
+        
+        public function getTimeZone()
+        {
+            return $this->timeZone;
+        }
+        
+        public function getCoordinates()
+        {
+            return $this->coordinates;
+        }
+        
+        public function getTypeId()
+        {
+            return $this->typeId;
+        }
+        
+        public function getDistance()
+        {
+            return $this->distance;
+        }
+        
+        public function getForecasts($weather = ["forecasts" => ["weather"]])
+        {
+            if (!isset($this->forecasts)) {
+                $this->fetch($weather);
+            }
+            
+            return $this->forecasts;
+        }
+
+        public function getObservationalGraphs($weather = ["observationalGraphs" => ["dew-point"]])
+        {
+            if (!isset($this->observationalGraphs)) {
+                $this->fetch($weather);
+            }
+            
+            return $this->observationalGraphs;
+        }
+        
+        public function getObservational()
+        {
+            if (!isset($this->observational)) {
+                $this->fetch(["observational" => true]);
+            }
+            
+            return $this->observational;
+        }
+    }
